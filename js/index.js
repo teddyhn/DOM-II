946c89e4fa9422f8bcf8f6da0e7f3409af5b434c
teddyhn <tngo97@gmail.com>
2019-08-06 14:14:33 -0700

    Initial commit

--- js/index.js
+++ js/index.js
@@ -1,1 +1,44 @@
 // Your code goes here
+
+// dblclick
+
+let busImg = document.querySelector('.intro img');
+
+busImg.addEventListener('dblclick', evt => {
+    evt.target.style.border = '2px solid gold';
+});
+
+
+// click, mouseover, mouseout
+
+let images = document.querySelectorAll('img');
+
+images.forEach(image => {
+    image.addEventListener('click', event => {
+        event.target.style.maxWidth = '200%';
+    });
+
+    image.addEventListener('mouseover', event => {
+        event.target.style.cursor = 'zoom-in';
+    });
+
+    image.addEventListener('mouseout', event => {
+        event.target.style.maxWidth = '100%';
+    });
+});
+
+// focus, keydown, keyup
+
+let destination = document.querySelector('.content-destination');
+
+destination.addEventListener('focus', evt => {
+    event.target.style.background = 'pink';
+});
+
+destination.addEventListener('keydown', evt => {
+    event.target.style.background = 'black';
+});
+
+destination.addEventListener('keyup', evt => {
+    event.target.style.background = 'yellow';
+});
